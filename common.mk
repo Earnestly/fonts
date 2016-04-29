6730214262cb0ac1b0fd4dc8c954c8afbeb273cb
Earnestly <zibeon@gmail.com>
2016-04-29 20:11:15 +0100

    A bunch of structural fixes

--- common.mk
+++ common.mk
@@ -1,8 +1,18 @@
-FONTFORGE = /usr/bin/fontforge
+PREFIX ?= /usr/local
+datarootdir ?= /share
+
+# This common makefile is (usually) included via a proper Makefile in a
+# sub-directory.  We determine the directory needed to find our scripts
+# relatively from this file.
+
+# For example: "include ../../common.mk" will assign "../../" to MKPATH.
+MKPATH := $(dir $(lastword $(MAKEFILE_LIST)))
+
+FONTFORGE = fontforge
 FONTFORGE_OPTIONS = -quiet -script
 
-VALIDATE.sfd = $(FONTFORGE) $(FONTFORGE_OPTIONS) ../mk/validate-opentype.pe
-COMPILE.otf = $(FONTFORGE) $(FONTFORGE_OPTIONS) ../mk/generate-opentype.pe
+VALIDATE.sfd = $(FONTFORGE) $(FONTFORGE_OPTIONS) $(MKPATH)/mk/validate-opentype.pe
+COMPILE.otf = $(FONTFORGE) $(FONTFORGE_OPTIONS) $(MKPATH)/mk/generate-opentype.pe
 
 %.otf: %.sfd
 	$(VALIDATE.sfd) $^
